0238f84c1b2cd3d6fdcb7004f5be80bc157f2043
mly-yy <uuhh44399@gmail.com>
2026-08-27 17:33:39 +0800

    新增菜品业务代码提交

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,18 @@
+package com.sky.mapper;
+
+import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DishFlavorMapper {
+
+    /**
+     * 批量插入口味数据
+     * @param flavors
+     */
+    static void insertBatch(List<DishFlavor> flavors) {
+
+    }
+}
