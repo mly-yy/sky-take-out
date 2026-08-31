48d06dd114d890287d48d8c23ed045c36ecd6e4f
mly-yy <uuhh44399@gmail.com>
2026-08-31 15:54:22 +0800

    套餐分页查询代码提交

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,7 +1,15 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface SetmealService {
     void saveWithDish(SetmealDTO setmealDTO);
+    /**
+     * 分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
