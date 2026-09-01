2d613bf5744be27dc67139130745c76ddb4587db
mly-yy <uuhh44399@gmail.com>
2026-09-01 15:01:53 +0800

    店铺营业状态设置代码提交

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,24 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+public class ShopController {
+    public static final String KEY ="SHOP_STATUS";
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @GetMapping("/status")
+    public Result<Integer> getStatus(){
+        Integer shopStatus =(Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取到店铺的营业状态为:{}",shopStatus==1?"营业中":"打烊中");
+        return Result.success(shopStatus);
+    }
+}
