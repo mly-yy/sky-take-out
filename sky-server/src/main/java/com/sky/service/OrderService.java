7cbba0d6b371156fc66668a4ee7dc5a8a6b0ca52
mly-yy <uuhh44399@gmail.com>
2026-09-14 21:32:53 +0800

    管理端拒单代码提交

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,9 +1,6 @@
 package com.sky.service;
 
-import com.sky.dto.OrdersConfirmDTO;
-import com.sky.dto.OrdersPageQueryDTO;
-import com.sky.dto.OrdersPaymentDTO;
-import com.sky.dto.OrdersSubmitDTO;
+import com.sky.dto.*;
 import com.sky.result.PageResult;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderStatisticsVO;
@@ -74,4 +71,10 @@ public interface OrderService {
      * @return
      */
     void confirm(OrdersConfirmDTO ordersConfirmDTO);
+    /**
+     * 拒单
+     * @param ordersRejectionDTO
+     * @return
+     */
+    void rejection(OrdersRejectionDTO ordersRejectionDTO) throws Exception;
 }
